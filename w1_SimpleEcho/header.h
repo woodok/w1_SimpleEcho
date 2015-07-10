f108d0da55344d1c696f602ab1e5af706c8f3480
W.D. Ok <newwoodok@gmail.com>
2015-07-10 16:56:45 +0900

    (nbw)

--- w1_SimpleEcho/header.h
+++ w1_SimpleEcho/header.h
@@ -9,6 +9,7 @@
 #include <list>
 #include <WinSock2.h>
 #include <algorithm>
+#include <string>
 
 namespace USER_INFO
 {
@@ -18,24 +19,20 @@ namespace USER_INFO
 class UserInfo
 {
 private:
-	static int createdObjNum;
-private:
 	int key;		// unique key for distinguishing user
-	char id[USER_INFO::ID_SIZE];	// login id
+	std::string id;		// login id
 	HANDLE hSocket;
 	int roomNumber;		// the number of room which the user currently participate. (Not in the room = 0)
 public:
-	UserInfo(char * _id, HANDLE _hSocket, int _roomNum) : roomNumber(_roomNum)
+	UserInfo(std::string _id, HANDLE _hSocket, int _roomNum = 0) : roomNumber(_roomNum)
 	{
-		key = ++createdObjNum;
-		strncpy_s(id, _id, USER_INFO::ID_SIZE);
+		id = _id;
 		hSocket = _hSocket;
 		std::cout << "UserInfo(..) called" << std::endl;
 	}
 	UserInfo() : roomNumber(0)
 	{
-		key = ++createdObjNum;
-		strncpy_s(id, "Default", USER_INFO::ID_SIZE);
+		id = "Default";
 		hSocket = INVALID_HANDLE_VALUE;
 		std::cout << "UserInfo() called" << std::endl;
 	}
@@ -45,16 +42,38 @@ public:
 	}
 	
 	// Accessor
-	const char * get_id() const;		//? 이렇게 되는 경우 혹시 리턴값으로부터 멤버 수정할 수도..?
+	const std::string& get_id() const;
 	int get_roomNumber() const;
 	int get_key() const;
 	HANDLE get_hSocket() const;
 
 	// Mutator
-	void set_id(char * _newid);
+	void set_id(std::string _newid);
 	void set_roomNumber(int _newRoomNum);
 	void set_key(int _newkey);
 	void set_hSocket(HANDLE _newhSocket);
+};
+
+// UserInfoList
+class UserInfoList
+{
+public:
+	std::list<UserInfo *> head;
+private:
+	int keyCount;
+public:
+	UserInfoList() : keyCount(0)
+	{}
+	~UserInfoList()
+	{
+		std::list<UserInfo*>::iterator it;
+		for (it = head.begin(); it != head.end(); ++it)
+			delete(*it);
+		std::cout << "UserInfoList object deleted" << std::endl;
+	}
+	void add(std::string _newid, HANDLE _hSocket, int _newRoomNum = 0);
+	void remove(int _key);
+	UserInfo& find(int _key);	
 };
 
 class ROOM_INFO
@@ -65,23 +84,19 @@ public:
 
 class RoomInfo
 {
-private:
-	static int createdObjNum;
 public:
 	int key;
-	char title[ROOM_INFO::TITLE_SIZE];
+	std::string title;
 	std::list<HANDLE> users;
 public:
-	RoomInfo(char * _title)
+	RoomInfo(std::string _title)
 	{
-		strncpy_s(title, _title, ROOM_INFO::TITLE_SIZE);
-		key = ++createdObjNum;
+		title = _title;
 		std::cout << "RoomInfo(..) called" << std::endl;
 	}
 	RoomInfo()
 	{
-		strncpy_s(title, "No title", ROOM_INFO::TITLE_SIZE);
-		key = ++createdObjNum;
+		title = "No title";
 		std::cout << "RoomInfo() called" << std::endl;
 	}
 	~RoomInfo()
@@ -94,35 +109,43 @@ public:
 
 	// Accessor
 	int get_key();
-	const char * get_title();
+	const std::string& get_title() const;
 	
 	// Mutator
-	void set_title(char * _newTitle);
+	void set_title(std::string _newTitle);
 };
-int RoomInfo::createdObjNum = 0;
 
-// UserInfoList
-class UserInfoList
+class RoomInfoList
 {
-public:
-	std::list<UserInfo *> head;
-private:
-	int keyCount;
-public:
-	void add(UserInfo *);
-	void remove(int _key);
-	UserInfo& find(int _key);
+	std::list<RoomInfo *> head;
+	//rev
 };
 
 // UserInfo
-const char* UserInfo::get_id() const	{ return id; }
+const std::string& UserInfo::get_id() const	{ return id; }
 int UserInfo::get_roomNumber() const	{ return roomNumber; }
 int UserInfo::get_key() const			{ return key; }
 HANDLE UserInfo::get_hSocket() const	{ return hSocket; }
-void UserInfo::set_id(char * _newid)	{ strncpy_s(id, _newid, USER_INFO::ID_SIZE); }
+void UserInfo::set_id(std::string _newid)	{ id = _newid; }
 void UserInfo::set_roomNumber(int _newRoomNumber)	{ roomNumber = _newRoomNumber; }
 void UserInfo::set_key(int _newkey)		{ key = _newkey; }
 void UserInfo::set_hSocket(HANDLE _newhSocket)		 { 	hSocket = _newhSocket; }
+
+// UserInfoList
+void UserInfoList::add(std::string _newid, HANDLE _hSocket, int _newRoomNum) 
+{
+	head.push_back(new UserInfo(_newid, _hSocket, _newRoomNum));
+}
+void UserInfoList::remove(int _key)
+{
+	RoomInfo& target = head.erase()
+
+
+}
+UserInfo& UserInfoList::find(int _key)
+{
+
+}
 
 // RoomInfo
 bool RoomInfo::joinUser(HANDLE _hSocket)
@@ -156,5 +179,5 @@ bool RoomInfo::operator==(RoomInfo& _comparedRoom)
 		return false;
 }
 int RoomInfo::get_key()		{ return key; }
-const char * RoomInfo::get_title()	{ return title; }
-void RoomInfo::set_title(char * _newTitle)	{ strncpy_s(title, _newTitle, ROOM_INFO::TITLE_SIZE); }
+const std::string& RoomInfo::get_title() const	{ return title; }
+void RoomInfo::set_title(std::string _newTitle)	{ title = _newTitle; }

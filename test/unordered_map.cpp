bdda144b67c962f6c1710c7cbbb7dee8719c7aa6
W.D. Ok <newwoodok@gmail.com>
2015-08-03 16:50:46 +0900

    (nbw)

---
+++ test/unordered_map.cpp
@@ -0,0 +1,36 @@
+
+
+#include <iostream>
+#include <WinSock2.h>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <unordered_map>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+	cout << "main() start" << endl;
+
+	std::unordered_map<int, std::vector<HANDLE>> um;
+	std::vector<HANDLE> vec{ INVALID_HANDLE_VALUE };
+
+	um.insert(std::unordered_map<int, std::vector<HANDLE>>::value_type(1, vec));
+	um.insert(std::unordered_map<int, std::vector<HANDLE>>::value_type(2, vec));
+	
+	um.find(2)->second.push_back(INVALID_HANDLE_VALUE);
+
+	//for_each(um.begin(), um.end(), [](std::unordered_map<int, std::vector<HANDLE>> & e) {
+	for_each(um.begin(), um.end(), [](std::pair<int, std::vector<HANDLE>> & e) {
+		cout << "key:" << e.first << "\t|";
+		int idx = 0;
+		for_each(e.second.begin(), e.second.end(), [&idx](HANDLE & e) {
+			cout << "vec[" << idx++ << "]:" << e << " >> ";
+		});
+		cout << " (end)" << endl;
+		
+	});
+}
